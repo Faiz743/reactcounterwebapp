02a919d7a2be98851b9c60e9cb5fecbab9a947aa
Faiz Akhtar <faizakhtar140@gmail.com>
2021-08-08 11:42:59 +0530

    my first commit reactcounterapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Button } from '@material-ui/core';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment = () => {
+    this.setState({count: this.state.count + 1});
+  }
+
+  decrement = () => {
+    this.setState({count: this.state.count - 1});
+  }
+
+  render() {
+    return (
+      <>
+        <div className="app">
+
+          <div className="welcome">
+            <h1>Welcome to Counter App</h1>
+          </div>
+
+          <div className="counter">
+            <h1>Counter</h1>
+          </div>
+
+          <div className="number">
+            <h2>{this.state.count}</h2>
+          </div>
+
+          <div className="button">
+            <Button variant="contained" size="large" color="primary" onClick={this.increment}>
+              Add Count
+            </Button>
+            <Button variant="contained" size="large" color="secondary" onClick={this.decrement}>
+              Lower Count
+            </Button>
+          </div>
+        
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
